469d100bdf23fa0bdc852517fd4f8f4a09abb9db
Samuel Muskovitz <samuel.muskovitz@gmail.com>
2022-06-06 11:12:34 -0400

    Working on chaning tile color on click funtion

---
+++ src/main/java/com/github/sammuskovitz/JPanelTile.java
@@ -0,0 +1,20 @@
+package com.github.sammuskovitz;
+
+import javax.swing.*;
+
+public class JPanelTile extends JPanel {
+
+    private Tile tile;
+
+    public JPanelTile(Tile tile){
+        this.tile = tile;
+    }
+
+    public Tile getTile() {
+        return tile;
+    }
+
+    public void setTile(Tile tile) {
+        this.tile = tile;
+    }
+}
--- src/main/java/com/github/sammuskovitz/Visualizer.java
+++ src/main/java/com/github/sammuskovitz/Visualizer.java
@@ -2,13 +2,20 @@ package com.github.sammuskovitz;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Visualizer {
 
-    public static JPanel convertTileToJPanel(Tile tile){
-        JPanel jPanel = new JPanel();
+    private static List<JPanelTile> allTiles = new ArrayList<>();
 
-        switch(tile.getTileCode()){
+
+    public static JPanelTile convertTileToJPanel(Tile tile){
+        JPanelTile jPanel = new JPanelTile(tile);
+
+        switch(jPanel.getTile().getTileCode()){
             case 'w':
                 jPanel.setBackground(Color.BLACK);
                 break;
@@ -22,11 +29,33 @@ public class Visualizer {
                 jPanel.setBackground(Color.WHITE);
         }
 
+
+
         return jPanel;
+    }
+
+    private static boolean isWithinArea(int x1, int y1, int x2, int y2, int width, int height){
+        return x1 >= x2 && x1 <= x2 + width && y1 >= y2 && y1 <= y2 + height;
     }
 
     public static JFrame setupJFrame(GameMap gameMap, int width, int height, String windowTitle){
         JPanel field = new JPanel(new GridLayout(gameMap.getMapWidth(), gameMap.getMapHeight()));
+        field.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int mouseX = e.getX();
+                int mouseY = e.getY();
+
+                Component[] tiles = field.getComponents();
+
+                for(int i = 0; i < allTiles.size(); i++){
+
+                    if(isWithinArea(mouseX, mouseY, tiles[i].getX(), tiles[i].getY(), tiles[i].getWidth(), tiles[i].getHeight())){
+                        tiles[i].setBackground(allTiles.get(i).getTile().getPaintColor());
+                    }
+                }
+            }
+        });
 
         for(Tile tile : gameMap.getAllTiles()){
             field.add(Visualizer.convertTileToJPanel(tile));
@@ -40,7 +69,5 @@ public class Visualizer {
 
         return frame;
     }
-
-
 }
 
